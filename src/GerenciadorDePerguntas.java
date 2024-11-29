5c509fa9f01647ebc8430f71977dfba3eafd35f0
Antonio Queiroz <antonioqueiroz.dev@gmail.com>
2024-11-28 21:16:28 -0300

    Primeiro comit

---
+++ src/GerenciadorDePerguntas.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GerenciadorDePerguntas {
+    String endereco = "C:\\Projects_java\\Desafio sistema de cadastros\\formulario.txt";
+
+
+    public  void lerFormulario(){
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(endereco))){
+            String linha;
+            while ((linha = reader.readLine()) != null){
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            System.out.println("Não foi possivel ler o arquivo: " + e.getMessage());
+        }
+    }
+
+    public  void cadastrarNovaPergunta() {
+        Scanner sc = new Scanner(System.in);
+
+        //*verifica o numero da ultima pergunta no formulario
+        int numeroQuestao = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(endereco))) {
+            while ((reader.readLine() != null)) {
+                numeroQuestao++;
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo" + e.getMessage());
+        }
+
+        System.out.println("Digite a nova pergunta: ");
+        var novaPergunta = sc.nextLine();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(endereco, true))) {
+            writer.newLine();
+            writer.write((numeroQuestao + 1) + " - " + novaPergunta);
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ão adicionar a nova questão!" + e.getMessage());
+        }
+
+    }
+
+    public void deletarPergunta() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Perguntas cadastradas.");
+        lerFormulario();
+
+        System.out.println("Digite o número da pergunta a ser deletada.");
+        int perguntaDeletar = sc.nextInt();
+
+        List<String> perguntas = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(endereco))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                perguntas.add(linha);
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler arquivo");
+        }
+
+        if (perguntaDeletar <= 4) {
+            System.out.println("As perguntas de 1 a 4 não podem ser deletadas!");
+        } else if (perguntaDeletar > perguntas.size()) {
+            System.out.println("pergunta inexistente");
+        } else {
+            perguntas.remove(perguntaDeletar - 1);
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(endereco))) {
+                for (String pergunta : perguntas) {
+                    writer.write(pergunta);
+                    writer.newLine();
+                }
+                System.out.println("pergunta deletada com sucesso!");
+            } catch (IOException e) {
+                System.out.println("Erro ão deletar pergunta! ");
+            }
+        }
+
+    }
+}
---
+++ src/GerenciadorDeUsuario.java
@@ -0,0 +1,106 @@
+import exceptions.*;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GerenciadorDeUsuario {
+    String endereco = "C:\\Projects_java\\Desafio sistema de cadastros\\formulario.txt";
+    List<Usuario> usuarios = new ArrayList<>();
+
+
+    public void cadastrarUsuario() {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String name = sc.nextLine();
+            if (name.length() < 10) {
+                throw new InvalidNomeException("O nome deve possuir no minimo 10 caracteres!");
+            }
+
+            String email = sc.nextLine();
+            if (!email.contains("@")) {
+                throw new IvalidEmailException("O email deve conter @");
+            }
+
+            for (Usuario usuario: usuarios){
+                if (usuario.getEmail().equals(email)){
+                    throw new EmailDuplicadoException("Este email já está cadastrado!");
+                }
+            }
+
+            int idade = sc.nextInt();
+            if (idade<18) {
+                throw new InvalidIdadeExcpetion("Idade minima 18 anos.");
+            }
+
+            sc.nextLine();
+
+            String altura = sc.nextLine();
+            if (!altura.contains(",")) {
+                throw new InvalidAlturaException("Altura inválida, use vírgula (ex: 1,80).");
+            }
+
+
+            Usuario usuario = new Usuario(name, email, idade, altura);
+
+            usuarios.add(usuario);
+
+
+            String enderecoArquivo = "C:\\Projects_java\\Desafio sistema de cadastros\\" + usuarios.size() +
+                    " - " + usuario.getName().toUpperCase() + ".txt";
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(enderecoArquivo))) {
+                writer.write(usuario.getName());
+                writer.newLine();
+                writer.write(usuario.getEmail());
+                writer.newLine();
+                writer.write(String.valueOf(usuario.getIdade()));
+                writer.newLine();
+                writer.write(String.valueOf(usuario.getAltura()));
+                writer.newLine();
+            } catch (IOException e) {
+                System.out.println("Erro ao salvar o arquivo" + e.getMessage());
+            }
+        }catch (InvalidNomeException | IvalidEmailException | EmailDuplicadoException |
+                InvalidIdadeExcpetion | InvalidAlturaException  e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public void listarUsuarios() {
+        if (usuarios.isEmpty()) {
+            System.out.println("Nenhum usuario encontrado!");
+        }else {
+            for (Usuario usuario : usuarios){
+                System.out.println(usuario.getName());
+            }
+        }
+    }
+
+    public void buscarUsuario(){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Digite o nome, idade ou email :");
+        String entrada = sc.nextLine();
+
+        boolean encontrado = false;
+        for (Usuario usuario : usuarios){
+            if (usuario.getName().toLowerCase().contains(entrada) ||
+                    String.valueOf(usuario.getIdade()).contains(entrada) ||
+                    usuario.getEmail().toLowerCase().contains(entrada)) {
+                System.out.println("Usuario encontrado:\n" + usuario);
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Usuário não encontrado.");
+        }
+
+    }
+
+}
